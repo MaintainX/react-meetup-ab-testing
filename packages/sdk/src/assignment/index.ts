b95a5f658580af43e26bfa6a195ba6fecd3229ca
Philippe Tessier <philippe.tessier@getmaintainx.com>
2024-01-09 18:09:29 -0500

    Rename hash file

--- packages/sdk/src/assignment/index.ts
+++ packages/sdk/src/assignment/index.ts
@@ -1,5 +1,5 @@
 export * from "./assignment-logger";
 export * from "./assignment-result";
 export * from "./bucket";
-export * from "./hashing";
+export * from "./hash";
 export * from "./query-string";
